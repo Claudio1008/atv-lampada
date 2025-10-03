beea4a5635b4aad752048029786eab62422c08ea
Claudio1008 <claudiogabrielbatista7@gmail.com>
2025-10-03 09:17:42 -0300

    atualizando a atividade

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let clienteWeb = null;
+let clientWeb = null;
 
 const clientId = "ESP8266" + Math.floor(Math.random() * 900) + 100;
-clienteWeb = new PaymentMethodChangeEvent.MQTT.Cliente("broker.hivemq.com", 8084, clientId);
+clientWeb = new Paho.MQTT.Client("broker.emqx.io", 8084, clientId);
+//clientWeb = new Paho.MQTT.Client("broker.hivemq.com", 8884, clientId);
 
-clienteWeb.connect({
+clientWeb.connect({
     useSSL: true,
     timeout: 5,
-    onSucess: function () {
-        alert('Conectado com sucesso!');
+    onSuccess: function(){
+        alert(`Conectado com sucesso!!`)
     },
-    onFailure: function(){
-        alert('A conexão falhou!')
+    onFailure: function (){
+        alert(`A conexão falhou!`)
     }
 })
 
@@ -19,14 +20,14 @@ function ligarAmarelo(){
 
     //fazendo publish no tópico, (broker)
     const msgAmar = new Paho.MQTT.Message("");
-    msgAmar.destinationName = "CGGB/led/ligado";
-    clienteWeb.send(msgAmar);
+    msgAmar.destinationName = "CGGB/led/on"
+    clientWeb.send(msgAmar)
 }
 
-function desligarAmarelo(){
+function desligar(){
     document.getElementById("amarelo").classList.remove("amar");
 
-    let msg = new Paho.MQTT.Message('');
-    msg.destinationName = "CGGB/led/desligado";
-    clienteWeb.send(msg);
+    let msg = new Paho.MQTT.Message(``);
+    msg.destinationName = "CGGB/led/off"
+    clientWeb.send(msg);
 }
